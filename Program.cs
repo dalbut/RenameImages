7f56740fffc75c4d865ba456247de8db973a6d47
dalbut <78615066+dalbut@users.noreply.github.com>
2022-04-23 17:45:48 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RenameImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = "";
+            string imagesFolder = "";
+            int counter = 0;
+
+            Console.WriteLine("Enter path to pictures folder: ");
+            imagesFolder = Console.ReadLine();
+
+            Console.WriteLine("Enter path to txt file with names: ");
+            fileName = Console.ReadLine();
+
+            List<string> names = new List<string>();
+
+
+            string text = File.ReadAllText(fileName + ".txt");
+            string[] lines = text.Split(Environment.NewLine);
+
+            foreach (string line in lines)
+            {
+                names.Add(line);
+            }
+
+            DirectoryInfo di = new DirectoryInfo($@"{imagesFolder}");
+            FileInfo[] files = di.GetFiles("*.png");
+
+            foreach (FileInfo file in files)
+            {
+                file.MoveTo($"{imagesFolder}/{names[counter]}.png");
+                counter++;
+            }
+        }
+    }
+}
